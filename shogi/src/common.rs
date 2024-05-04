3fdea523721e6b87431f972b2faf5b13a235d801
applyuser160 <syunsuke130121@gamil.com>
2024-05-04 15:50:38 +0900

    update common

--- shogi/src/common.rs
+++ shogi/src/common.rs
@@ -1,7 +1,15 @@
-pub fn alphabet_to_number(alphabet: char) -> u8 {
-    return alphabet as u8;
+pub fn alphabet_to_number(alphabet: char) -> char {
+    return ((alphabet as u8) - 48) as char;
 }
 
-pub fn number_to_alphabet(number: u8) -> char {
-    return (number + 48) as char;
+pub fn number_to_alphabet(number: char) -> char {
+    return ((number as u8) + 48) as char;
+}
+
+pub fn char_to_integer(character: char) -> u8 {
+    return (character as u8) - 48;
+}
+
+pub fn integer_to_char(integer: u8) -> char {
+    return (integer + 48) as char;
 }
