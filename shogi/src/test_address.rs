1ad208030b22e4cd836b75237f7d175c24fe55e5
applyuser160 <syunsuke130121@gamil.com>
2024-05-28 22:35:15 +0900

    add test address and common

---
+++ shogi/src/test_address.rs
@@ -0,0 +1,67 @@
+#[cfg(test)]
+
+mod tests {
+    use crate::address::{address_to_index, index_to_address, index_to_column, index_to_row, Address};
+
+    #[test]
+    fn case01() {
+        let address = Address::from_number(57);
+        assert_eq!(address.column, 2);
+        assert_eq!(address.row, 5);
+    }
+
+    #[test]
+    fn case02() {
+        let address = Address::from_numbers(5, 9);
+        assert_eq!(address.column, 5);
+        assert_eq!(address.row, 9);
+    }
+
+    #[test]
+    fn case03() {
+        let address = Address::from_string("9i");
+        assert_eq!(address.column, 9);
+        assert_eq!(address.row, 9);
+    }
+
+    #[test]
+    fn case04() {
+        let address = Address::from_string("9i");
+        let index = address.to_index();
+        assert_eq!(index, 108);
+    }
+
+    #[test]
+    fn case05() {
+        let address = Address::from_string("9i");
+        let str = address.to_string();
+        assert_eq!(str, String::from("9i"))
+    }
+
+    #[test]
+    fn case06() {
+        let address = index_to_address(100);
+        assert_eq!(address.column, 1);
+        assert_eq!(address.row, 9);
+    }
+
+    #[test]
+    fn case07() {
+        let address = index_to_address(100);
+        let index = address_to_index(address);
+        assert_eq!(index, 100);
+    }
+
+    #[test]
+    fn case08() {
+        let row = index_to_row(100);
+        assert_eq!(row, 9);
+    }
+
+    #[test]
+    fn case09() {
+        let row = index_to_column(100);
+        assert_eq!(row, 1);
+    }
+
+}
---
+++ shogi/src/test_common.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+
+mod tests {
+    use crate::common::{alphabet_to_number, char_to_integer, integer_to_char, number_to_alphabet};
+
+
+    #[test]
+    fn case01() {
+        let num = alphabet_to_number('a');
+        assert_eq!(num, '1');
+    }
+
+    #[test]
+    fn case02() {
+        let ch = number_to_alphabet('2');
+        assert_eq!(ch, 'b');
+    }
+
+    #[test]
+    fn case03() {
+        let code = char_to_integer('3');
+        assert_eq!(code, 3);
+    }
+
+    #[test]
+    fn case04() {
+        let ch = integer_to_char(8);
+        assert_eq!(ch, '8');
+    }
+
+}
