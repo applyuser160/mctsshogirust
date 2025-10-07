73e5bf60554e6376cd52a31f27eeff08c01710bb
applyuser160 <syunsuke130121@gamil.com>
2025-10-07 19:46:33 +0900

    update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ language = "ja"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_material"
 html_static_path = ["_static"]
 
 # -- Extension configuration -------------------------------------------------
